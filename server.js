d8d890c608f0621a15a78e9a66c6184ef1c124b2
Sergey <gvsergey89@gmail.com>
2024-12-07 13:46:21 +0300

    x

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors({
 app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.static('public'));
-const port = process.env.PORT || 3000;
+const port =  3000;
 const server = http.createServer(app);
 
 app.get('/getData', async (req, res) => {
